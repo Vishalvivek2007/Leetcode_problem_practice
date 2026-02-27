a2a6fad56330d02cc49c9e64796712df18b5f3d3
Vishal Vivek <vishal.vivek.10039@gmail.com>
2026-02-27 14:28:16 +0530

    Same Tree(Easy)

---
+++ sametree.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p==NULL && q==NULL){
+        return true;
+    }
+    else if(p==NULL && q!=NULL || p!=NULL && q==NULL){
+        return false;
+    }
+
+    if(p->val!=q->val){
+        return false;
+    }
+    bool left=isSameTree(p->left, q->left);
+    bool right=isSameTree(p->right, q->right);
+    if(left && right){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
