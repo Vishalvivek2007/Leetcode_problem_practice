730751c10f22930f396283cc0358039e8f01b4f1
Vishal Vivek <vishal.vivek.10039@gmail.com>
2026-08-23 20:09:03 +0530

    Partition Equal Subset Sum (MEDIUM)

---
+++ patrialequall.c
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+      bool canPartition(vector<int>& nums) {
+          int total = 0;
+  
+          for(int x : nums)
+              total += x;
+  
+          if(total % 2 != 0)
+              return false;
+  
+          int target = total / 2;
+  
+          vector<bool> dp(target + 1, false);
+          dp[0] = true;
+  
+          for(int x : nums) {
+              for(int j = target; j >= x; j--) {
+                  dp[j] = dp[j] || dp[j - x];
+              }
+          }
+  
+          return dp[target];
+      }
+  };
